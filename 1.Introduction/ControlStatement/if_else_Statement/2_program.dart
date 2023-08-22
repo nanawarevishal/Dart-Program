d3ede73105b6b325394ed54e58677c22934515d7
nanawarevishal <nanawarevishal18@gmail.com>
2023-08-22 09:50:51 +0530

    Control Statements

---
+++ 1.Introduction/ControlStatement/if_else_Statement/2_program.dart
@@ -0,0 +1,20 @@
+
+
+void main(){
+
+    int x =5;
+    int y =7;
+
+    if((++x <= --y) || (x++ > ++y)){
+        print("Both same");
+    }
+
+    else{
+
+        print("Not Same");
+    }
+
+    print(x);
+    print(y);
+
+}
---
+++ 1.Introduction/ControlStatement/if_else_Statement/4_program.dart
@@ -0,0 +1,18 @@
+
+
+void main(){
+    
+    int num = -7;
+
+    if(num>0){
+        print("Num is positive");
+    }
+
+    else if(num<0){
+        print("Num is negative");
+    }
+
+    else{
+        print("num is zero");
+    }
+}
