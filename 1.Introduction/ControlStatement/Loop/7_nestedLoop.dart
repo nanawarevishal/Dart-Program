2134a963711bdaab161df54b39cc58a5795c680c
nanawarevishal <nanawarevishal18@gmail.com>
2023-08-25 11:53:28 +0530

    Nested Loop

---
+++ 1.Introduction/ControlStatement/Loop/7_nestedLoop.dart
@@ -0,0 +1,20 @@
+
+
+import 'dart:io';
+
+void main(){
+
+    int count =9;
+
+    for(int i=1;i<=3;i++){
+
+        for(int j=1;j<=3;j++){
+
+            stdout.write("${count} ");
+            count--;
+        
+        }
+        
+        print(" ");
+    }
+}
