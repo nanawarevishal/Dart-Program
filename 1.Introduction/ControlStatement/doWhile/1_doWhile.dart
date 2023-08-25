039ed27a91d7739fd5a44b66e4cee4c56a8430ea
nanawarevishal <nanawarevishal18@gmail.com>
2023-08-25 11:52:17 +0530

    doWhile

---
+++ 1.Introduction/ControlStatement/doWhile/1_doWhile.dart
@@ -0,0 +1,33 @@
+
+
+void main(){
+
+    int x =1;
+
+    do{
+
+        print("1.C");
+        print("2.CPP");
+        print("3.Java");
+        print("4.dart");
+
+
+        switch (x) {
+          case 1:
+            print("1972");
+            
+          case 2:
+            print("1978");
+            
+          case 3:
+            print("1995");
+            
+          case 4:
+            print("2013");
+            
+
+          default:
+            print("No Match Found");
+        }
+    }while(false);
+}
