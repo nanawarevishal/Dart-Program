588dc157b51e4d048350059db42e70ce2b86ca5d
nanawarevishal <nanawarevishal18@gmail.com>
2023-08-28 09:49:07 +0530

    functions

---
+++ 1.Introduction/Functions/1_userFunction.dart
@@ -0,0 +1,15 @@
+
+
+void fun(){
+
+    print("In Fun");
+}
+
+
+void main(){
+
+    print("Start Main");
+    fun();
+
+    print('End Main');
+}
---
+++ 1.Introduction/Functions/4_userFunction.dart
@@ -0,0 +1,17 @@
+
+
+
+
+void fun(String name,double salary){
+    print("In Fun");
+    print("Name: ${name}");
+    print("Salary: ${salary}");
+}
+
+void main(){
+
+    print("Start Main");
+
+    fun("Kanha",20.7);
+    print("End main");
+}
