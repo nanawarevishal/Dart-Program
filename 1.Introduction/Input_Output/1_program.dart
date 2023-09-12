66f6cc88b73577fc346ab6e207646ae963b73f16
nanawarevishal <nanawarevishal18@gmail.com>
2023-09-12 20:19:20 +0530

    Input Output

---
+++ 1.Introduction/Input_Output/1_program.dart
@@ -0,0 +1,21 @@
+
+
+import 'dart:io';
+
+void main(){
+
+    print(stdin.runtimeType);
+    
+
+    stdout.write("Enter the name: ");
+    String name = stdin.readLineSync()!;
+    print("Name: ${name}");
+
+    stdout.write("Enter the age: ");
+    int age = int.parse(stdin.readLineSync()!);
+    print("Age: ${age}");
+
+    stdout.write("Enter the Salary: ");
+    double Salary = double.parse(stdin.readLineSync()!);
+    print("Salary: ${Salary}");
+}
