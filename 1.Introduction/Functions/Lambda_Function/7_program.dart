d0f4edb3d60ae57f81ad6161dc17d4f4d5a31256
nanawarevishal <nanawarevishal18@gmail.com>
2023-09-13 10:19:35 +0530

    lambda Function

---
+++ 1.Introduction/Functions/Lambda_Function/7_program.dart
@@ -0,0 +1,17 @@
+
+
+
+void main(){
+
+    int x =10;
+    int y =20;
+
+    Null Function(int a) fun = (int a){
+        print(x);
+        print(y);
+        print(a);
+    };
+
+    fun(20);
+
+}
