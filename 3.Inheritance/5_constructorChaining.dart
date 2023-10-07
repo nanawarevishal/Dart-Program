ea3ff4efff092573aaf38a2e16f08c717627875d
nanawarevishal <nanawarevishal18@gmail.com>
2023-10-07 19:13:56 +0530

    Inheritance

--- 3.Inheritance/5_constructorChaining.dart
+++ 3.Inheritance/5_constructorChaining.dart
@@ -6,11 +6,18 @@ class Company{
     String? compName;
     String? loc;
 
-    // Company(){
-    //     print("In parent constrctor");
-    // }
+    Company(){
+        // this();
+        print("In parent constrctor");
+    }
 
-    Company(this.compName,this.loc);
+    Company.cons(this.compName,this.loc){
+        this();
+    }
+
+    call(){
+        Company();
+    }
 
     void compInfo(){
         print(compName);
@@ -23,7 +30,7 @@ class Employee extends Company{
     int? empID;
     String? empName;
 
-    Employee(this.empID,this.empName):super("Biencaps","Pune");
+    Employee(this.empID,this.empName):super.cons("Biencaps","Pune");
 
     void empInfo(){
         print(empID);
