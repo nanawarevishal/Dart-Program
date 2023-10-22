4d609602060a01d633b0e3ccbf5e91f257072be5
nanawarevishal <nanawarevishal18@gmail.com>
2023-10-22 09:22:59 +0530

    Abstract Factory Constructor

---
+++ 2.Class_and_Objects/Constructor/22_AbstarctFactoryConstructor.dart
@@ -0,0 +1,58 @@
+
+
+
+abstract class Developer{
+
+    factory Developer(String devType){
+        
+        if(devType == "Backend")
+            return Backend();
+
+        else if (devType == "FrontEnd"){
+            return FrontEnd();
+        } 
+
+        else {
+            return Mobile();
+        }
+    }
+
+    void devLang();
+}
+
+class Backend extends Developer{
+
+    void devLang(){
+        print("Java/NodeJS");
+    }
+}
+
+class FrontEnd implements Developer{
+
+    void devLang(){
+        print("HTML/CSS/Javascript");
+    }
+}
+
+class Mobile implements Developer{
+
+    void devLang(){
+        print("Flutter/Android");
+    }
+}
+
+
+void main(){
+
+    Developer obj = new Developer("Backend");
+    obj.devLang();
+}
+
+
+/*
+
+Output:
+    22_AbstarctFactoryConstructor.dart:14:7: Error: The superclass, 'Developer', has no unnamed constructor that takes no arguments.    
+    class Backend extends Developer{
+      ^
+*/
