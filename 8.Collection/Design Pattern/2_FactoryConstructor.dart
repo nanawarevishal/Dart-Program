89aab3d718c25f9b7fa6130f2a05c7db7f98722c
nanawarevishal <nanawarevishal18@gmail.com>
2023-10-25 23:25:35 +0530

    Design Pattern

--- 8.Collection/Design Pattern/2_FactoryConstructor.dart
+++ 8.Collection/Design Pattern/2_FactoryConstructor.dart
@@ -2,7 +2,7 @@
 
 class Singleton{
 
-    static Singleton obj = new Singleton();
+    static Singleton obj = new Singleton._private();
 
     Singleton._private(){
         print("In Constructor");
